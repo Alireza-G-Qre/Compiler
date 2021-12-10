252440ca4e71579fa424bce01abe9d97ce39b1d5
Alireza-G-Qre <Alireza.g.qre@gmail.com>
2021-12-10 10:54:34 +0330

    End

--- src/compiler.py
+++ src/compiler.py
@@ -1,5 +1,3 @@
-import os
-
 from scanner import Scanner
 from parser import Parser
 
@@ -14,15 +12,6 @@ class Compiler:
         self.parser.proc()
         return self
 
-    def document(self):
-        self.parser.tree.save2file(key=False, filename='parse_tree.txt')
-        with open('syntax_errors.txt', 'w') as f:
-            f.writelines(
-                F'#{x["lineno"]} : syntax error, {x["message"]}\n'
-                for x in self.parser.errors)
-
-        return self
-
 
 if __name__ == '__main__':
-    Compiler('input.txt').compile().document()
+    Compiler('input.txt').compile()
